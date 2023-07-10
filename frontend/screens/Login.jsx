e8e8598d6b5c5733aa384e67d5cbf632e7aa77aa
cclshub <97969453+cclshub@users.noreply.github.com>
2023-07-10 13:04:00 +0800

    finally resolved merge conflict in login

--- frontend/screens/Login.jsx
+++ frontend/screens/Login.jsx
@@ -21,25 +21,6 @@ const Login = () => {
       //  <KeyboardAvoidingView style={styles.container} behavior="position">
       <View style={styles.container}>
         <Text style={styles.headertext}>Log In</Text>
-        <View style={styles.verticalcontainer}>
-          <Text style={styles.labeltext}>Email</Text>
-          <MyTextInput placeholder="Enter your email" />
-        </View>
-        <View style={styles.verticalcontainer}>
-          <Text style={styles.labeltext}>Password</Text>
-          <MyTextInput placeholder="Set up a password" isPassword={true} />
-        </View>
-        <View>
-          <TouchableOpacity style={styles.createaccount} onPress={() => navigate.navigate('main-nav')}>
-            <Text style={styles.createaccounttext}>Log In</Text>
-          </TouchableOpacity>
-        </View>
-  
-        <View style={styles.horizontalcontainer}>
-          <Text>Don't have an account?</Text>
-          <TouchableOpacity onPress={() => navigate.navigate('email-register')}>
-            <Text style={styles.logintext}>Register</Text>
-          </TouchableOpacity>
         <View style={styles.formcontainer}>
           <View style={styles.verticalcontainer}>
             <Text style={styles.labeltext}>Email</Text>
@@ -50,10 +31,9 @@ const Login = () => {
             <MyTextInput placeholder="Set up a password" isPassword={true} />
           </View>
         </View>
-
         <View style={styles.bottomcontainer}>
           <View>
-            <TouchableOpacity style={styles.createaccount}>
+            <TouchableOpacity style={styles.createaccount}  onPress={() => navigate.navigate('main-nav')}>
               <Text style={styles.createaccounttext}>Log In</Text>
             </TouchableOpacity>
           </View>
@@ -65,7 +45,6 @@ const Login = () => {
           </View>
         </View>
       </View>
-    </View>
       // </KeyboardAvoidingView>
     );
   };
